ffa010cd375d76d520b691b7454017aeb34031cb
lebuckman <liambuckman0@gmail.com>
2025-07-09 21:53:22 -0700

    Refactor random color picker code

--- script.js
+++ script.js
@@ -78,8 +78,7 @@ function changeColor() {
     } else if (eraserRadio.checked) {
         this.style.backgroundColor = "";
     } else if (randomRadio.checked) {
-        let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-        this.style.backgroundColor = "#" + randomColor;
+        this.style.backgroundColor = getRandomRGB();
     }
 }
 
@@ -105,4 +104,11 @@ function showSizeError() {
 
 function entryHint() {
     sizePrompt.textContent = `Enter a number between 2 and 99.`;
+}
+
+function getRandomRGB() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
 }
